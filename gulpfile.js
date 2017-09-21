d41082d60bccd4c8e07613bf6ba53bcdf61158bb
李帆 <18650715138@163.com>
2017-09-21 17:22:22 +0800

    首页
    做了部分首页

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var cleanCss = require('gulp-clean-css');
 var rename = require('gulp-rename');
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: ['./scss/**/*.scss','./www/views/**/*.scss']
 };
 
 gulp.task('default', ['sass']);
